cab9f1ad6c28f8be080db079000083542f7c8876
marckimbrey <marckimbrey@gmail.com>
2020-10-21 06:59:14 +0100

    move snake functionality

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-let direction;
-let snake = [{x: 35,y: 25}];
-const grid = generateGrid();
-appleLocation(grid);
-grid[snake[0].x][snake[0].y] = 'S';
+let direction = {x:1, y:0}; //default to right
+let snake;
+let grid;
 
-// generate 2d array
+// initial gameState
 function generateGrid() {
   const arr = [];
   for (let x = 0;x<75; x++) {
@@ -16,7 +14,11 @@ function generateGrid() {
       arr[x].push(0);
     }
   }
-  return arr
+  snake = [{x: 35,y: 25}];
+  grid = arr;
+  grid[snake[0].x][snake[0].y] = 'S';
+  appleLocation(grid);
+  draw(grid)
 }
 
 function appleLocation(arr) {
@@ -31,6 +33,66 @@ function appleLocation(arr) {
 }
 
 // game turn
+function gameTurn() {
+  setInterval(() => {
+    checkDestination()
+  },500)
+
+  //  check destination
+
+  //  update board and snake
+}
+
+function checkDestination() {
+  const newhHeadX = snake[0].x + direction.x;
+  const newHeadY = snake[0].y + direction.y;
+  switch (grid[newhHeadX][newHeadY]) {
+    case 0:
+      // move snake
+      ctx.fillStyle = 'green';
+      snake.unshift({x:newhHeadX, y:newHeadY});
+      grid[newhHeadX][newHeadY] = 'S';
+      ctx.fillRect(newhHeadX*8, newHeadY*8, 8,8);
+      // update tail
+      ctx.fillStyle = 'black';
+      const oldTail = snake.pop();
+      grid[oldTail.x][oldTail.y] = 0;
+      ctx.fillRect(oldTail.x*8, oldTail.y*8, 8,8);
+      break;
+    case 'S':
+      // gameOver
+      break;
+    case 'A':
+      // eat apple
+      // generate new apple
+      break;
+    default:
+      // out of bounds
+      // gameOver
+      break;
+
+  }
+}
+
+
+
+function changeDirection(e) {
+  switch(e.keyCode) {
+    case 37: // left
+      direction ={x:-1, y:0};
+      break;
+      case 38: // up
+        direction ={x:0, y:-1};
+        break;
+      case 39: // right
+        direction ={x:1, y:0};
+        break;
+      case 40: // down
+        direction ={x:0, y: 1};
+        break;
+
+  }
+}
 
 // draw
 function draw(arr) {
@@ -49,4 +111,8 @@ function draw(arr) {
     }
   }
 }
-draw(grid)
+
+generateGrid() // initial gameState
+gameTurn();
+
+document.addEventListener('keydown',changeDirection )
